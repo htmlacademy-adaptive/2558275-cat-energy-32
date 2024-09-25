c8b6323167180fe9ebacfb5ef918908d69749d67
RuslanGnlln <ruslan040506@gmail.com>
2024-09-25 20:42:23 +0200

    Сетка навигации

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,12 @@
 /* в этот файл добавляет скрипты*/
+const button = document.querySelector('#button');
+const nav = document.querySelector('#nav');
+const mainNav = document.querySelector('#main-nav');
+
+button.classList.remove('main-nav__toggle--close');
+mainNav.classList.remove('main-nav--no-js');
+
+button.addEventListener('click', () => {
+  button.classList.toggle('main-nav__toggle--close');
+  nav.classList.toggle('nav-list--open');
+});
